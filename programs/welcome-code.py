f753f321e7730f3c08c94686555dd303bc5baa9b
Madhesh V <83573352+Madhesh-V@users.noreply.github.com>
2023-10-01 16:27:59 +0530

    Create welcome-code.py

---
+++ programs/welcome-code.py
@@ -0,0 +1,13 @@
+# Welcome message for Hacktoberfest contributors
+def welcome_contributor(username):
+    print(f"Welcome, {username}!")
+    print("Thank you for contributing to Hacktoberfest.")
+    print("Happy coding!")
+
+# List of contributors
+contributors = ["Alice", "Bob", "Charlie", "David"]
+
+# Welcome each contributor
+for contributor in contributors:
+    welcome_contributor(contributor)
+    print("\n")  # Add a newline for separation
